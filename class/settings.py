ff41f99598bb3f80dd98448dcf9e88f2fc3456b3
xiaoxd16 <xiaoxd16@163.com>
2019-01-21 00:47:08 +0800

    fix bug : config --> configs

--- class/settings.py
+++ class/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIGS = json.loads(open(os.path.join(BASE_DIR, 'config.json')).read())
+CONFIGS = json.loads(open(os.path.join(BASE_DIR, 'configs.json')).read())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
